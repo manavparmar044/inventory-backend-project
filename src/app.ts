2084f97c95b3bd01c279f57566dff67aaa4f6db4
Manav Manish Parmar <mmparmar044@gmail.com>
2024-08-22 23:14:37 +0530

    User routed created

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { productRoutes } from "./app/modules/products/productRoutes"
 import { orderRoutes } from "./app/modules/orders/orderRoutes"
+import { userRoutes } from "./app/modules/users/userRoutes"
 const app = express()
 
 app.use(express.json())
@@ -9,6 +10,7 @@ app.use(cors())
 
 app.use("/api/products", productRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/users", userRoutes)
 
 app.listen(8000, ()=>{
     console.log("Listening");    
---
+++ src/app/modules/users/userRoutes.ts
@@ -0,0 +1,6 @@
+import express from "express"
+const router = express.Router()
+
+router.post("/create-account")
+
+export const userRoutes = router
